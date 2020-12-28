60f011172a952477b3c58c397a0eb2121f4456e2
Merlyn Bolle <merlyn.bolle@gmail.com>
2020-12-28 23:30:08 +0100

    Code PHP

---
+++ exercices/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Exercice 1 de PHP</h1>
+
+
+<h2>Exemple a)</h2>
+
+<?php
+    echo"Hello World !";
+?>
+
+<h2>Exemple b)</h2>
+
+<?php 
+    $textDuParagraphe = "Hello World!";
+    $entier = 7;
+    $decimal = 10.5; 
+    $x = "12.5io1"; // reconnaît les premiers caractères comme étant un type décimal
+    $y = 4;
+    $z = 5;
+
+    define("VITESSE_MAX",300000);
+    define("AGE_LIMITE", 14);
+
+    // echo $textDuParagraphe;
+    // echo $entier;
+    // echo $decimal;
+
+    // echo $VITESSE_MAX; est une erreur car ce n'est PAS une VARIABLE mais une CONSTANTE
+    echo VITESSE_MAX
+    echo " ";
+    echo constant("VITESSE_MAX");
+
+    echo $z;
+?>
+
+</body>
+</html>
