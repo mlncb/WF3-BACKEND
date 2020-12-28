ec2d6ea94e5fe386a01f41f94fd27ecf30ac3790
Merlyn Bolle <merlyn.bolle@gmail.com>
2020-12-29 00:02:14 +0100

    Main Code PHP

--- exercices/index.php
+++ exercices/index.php
@@ -17,21 +17,16 @@
     $textDuParagraphe = "Hello World!";
     $entier = 7;
     $decimal = 10.5; 
-    $x = "3"; 
+    $x = 3; 
     $y = 4;
     $z = 5;
 
-    define("VITESSE_MAX", 300000);
-    define("AGE_LIMITE", 14);
 
     // echo $textDuParagraphe;
     // echo $entier;
     // echo $decimal;
 
-    //echo$VITESSE_MAX; est une erreur car ce n'est PAS une VARIABLE mais une CONSTANTE 
-    echo VITESSE_MAX;
-    echo " ";
-    echo constant("VITESSE_MAX");
+    echo$z
 ?>
 
 </body>
